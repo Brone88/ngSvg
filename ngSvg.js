fd34feef89912093f174ce8bbcb79f05eec4de24
Br1 <br.one988@gmail.com>
2014-12-30 21:11:35 +0100

    ie8 switch

--- ngSvg.js
+++ ngSvg.js
@@ -1,22 +1,36 @@
 'use strict';
 
+function resolveTemplate(tElement, tAttrs) {
+    if(!Modernizr.svg) {
+        if(tAttrs.ie){
+            return '<img src="img/' + tAttrs.useid + '_' + tAttrs.ie + '.png" />'
+        } else {
+            return '<img src="img/' + tAttrs.useid + '.png" />'
+        }
+    } else {
+        return '<svg xmlns="http://www.w3.org/2000/svg" version="1.1"><use xlink:href=""/></svg>'
+    }
+}
+
 angular.module('ngSvg', [])
 
     .directive('usesvg', [function(){
         return {
             restrict : 'E',
-            replace: true,
+            replace:true,
             controller : ['$scope',  function($scope){
 
             }],
             scope: {
-                useid:'@'
+                useid:'@',
+                ie:'@'
             },
-            template: '<svg><use xlink:href=""/> </svg>',
+            template: resolveTemplate,
             link:function(scope, svg, attrs){
-
-                svg.attr('viewBox', document.getElementById(attrs.useid).getAttribute('viewBox'));
-                svg.children().attr('xlink:href', '#' + attrs.useid);
+                if(svg.prop('tagName') == 'svg'){
+                    svg.attr('viewBox', document.getElementById(attrs.useid).getAttribute('viewBox'));
+                    svg.children().attr('xlink:href', '#' + attrs.useid);
+                }
             }
         };
     }])
